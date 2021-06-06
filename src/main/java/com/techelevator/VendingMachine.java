23391e26c82d4d6496063ecd0692911b42d4f743
Shyngis Zhalitov <shzhalitov@gmail.com>
2021-06-06 14:54:38 -0400

    Unit testing work started

--- src/main/java/com/techelevator/VendingMachine.java
+++ src/main/java/com/techelevator/VendingMachine.java
@@ -1,11 +1,8 @@
 package com.techelevator;
 
-import com.techelevator.view.Menu;
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
@@ -16,36 +13,39 @@ public class VendingMachine {
 
     private Map<String, Item> inventory = new HashMap<>();
     private double currentBalance = 0.00;
-    private String destinationFile = "src/test/resources/Log.txt";
-    private File destination = new File(destinationFile);
     private String currentDateTime = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss aa").format(new Date());
 
 
 
 
 
-    private Map getVendingMachineStocked(String inventoryFilePath) {
+    public Map getVendingMachineStocked(String inventoryFilePath) {
         File inputFile = new File(inventoryFilePath);
         try (Scanner inputScanner = new Scanner(inputFile)) {
             while (inputScanner.hasNextLine()) {
                 String line = inputScanner.nextLine();
-                String[] product = line.split("\\|");
-                String productSlot = product[0];
-                String productName = product[1];
-                double productPrice = Double.parseDouble(product[2]);
-                String productCategory = product[3];
-                if (productCategory.equals("Chip")) {
-                    Chip item = new Chip(productSlot, productName, productPrice);
-                    inventory.put(productSlot, item);
-                } else if (productCategory.equals("Candy")) {
-                    Candy item = new Candy(productSlot, productName, productPrice);
-                    inventory.put(productSlot, item);
-                } else if (productCategory.equals("Drink")) {
-                    Drink item = new Drink(productSlot, productName, productPrice);
-                    inventory.put(productSlot, item);
+                if (line.contains("|")) {
+                    String[] product = line.split("\\|");
+                    String productSlot = product[0];
+                    String productName = product[1];
+                    double productPrice = Double.parseDouble(product[2]);
+                    String productCategory = product[3];
+                    if (productCategory.equals("Chip")) {
+                        Chip item = new Chip(productSlot, productName, productPrice);
+                        inventory.put(productSlot, item);
+                    } else if (productCategory.equals("Candy")) {
+                        Candy item = new Candy(productSlot, productName, productPrice);
+                        inventory.put(productSlot, item);
+                    } else if (productCategory.equals("Drink")) {
+                        Drink item = new Drink(productSlot, productName, productPrice);
+                        inventory.put(productSlot, item);
+                    } else {
+                        Gum item = new Gum(productSlot, productName, productPrice);
+                        inventory.put(productSlot, item);
+                    }
                 } else {
-                    Gum item = new Gum (productSlot, productName, productPrice);
-                    inventory.put(productSlot, item);
+                    System.out.println("This file doesn't contain products required to list!");
+                    break;
                 }
             }
         } catch (IOException e) {
@@ -55,52 +55,23 @@ public class VendingMachine {
     }
 
 
-    public double feedMe(double currentBalance){
-        Scanner scan = new Scanner(System.in);
-        String amountInput;
-        int amountProvided = 0;
-        String  x = "";
-        double reportBalance = currentBalance;
-        while(!x.equals("0")) {
-
+    public double feedMe(String amountInput){
             try {
-                System.out.println("Current Balance: $" + currentBalance);
-                System.out.println("How much are you entering");
-                amountInput = scan.nextLine();
                 Double amount = Double.valueOf(amountInput);
                 if (amount == 1) {
                     currentBalance = getCurrentBalance() + amount;
-                    amountProvided += amount;
                 } else if (amount == 2) {
                     currentBalance = getCurrentBalance() + amount;
-                    amountProvided += amount;
                 } else if (amount == 5) {
                     currentBalance = getCurrentBalance() + amount;
-                    amountProvided += amount;
                 } else if (amount == 10) {
                     currentBalance = getCurrentBalance() + amount;
-                    amountProvided += amount;
                 } else {
-                    System.out.println("You have provided unacceptable amount! We only accept $1, $2, $5 or $10!");
+                    currentBalance = 0.0;
                 }
-                System.out.println("Current Balance is: $ " + currentBalance);
-                Scanner answer = new Scanner(System.in);
-                System.out.println("Press 0 to return to menu or any key to add more money");
-                x = answer.nextLine();
-            } catch (NumberFormatException ex) {
-                System.out.println("You have provided unacceptable amount! We only accept $1, $2, $5 or $10!");
-            }
-        }
-        System.out.println("Current Money Provided: $" + amountProvided);
-        System.out.println("Current Balance: $" + currentBalance);
-        try {
-            PrintWriter writer = new PrintWriter(new FileWriter(destination, true));
-            writer.println(currentDateTime + " FEED MONEY: $" + reportBalance + " $" + currentBalance);
-            writer.flush();
-        } catch(IOException e) {
-            System.out.println("There is an error: " + e);
-        }
+            }catch (Exception e) {
 
+            }
         return currentBalance;
 
     }
@@ -126,13 +97,7 @@ public class VendingMachine {
                         System.out.print(item.getName() + " | " + item.getPrice() + " | " + "Your remaining balance is: $");
                         System.out.printf(" %.2f", currentBalance);
                         System.out.println(" ");
-                        try {
-                            PrintWriter writer = new PrintWriter(new FileWriter(destination, true));
-                            writer.println(">" + currentDateTime + " " + item.getName() + " " + item.getSlotNum() + " $" + item.getPrice());
-                            writer.flush();
-                        } catch(IOException e) {
-                            System.out.println("There is an error: " + e);
-                        }
+
                     } else {
                         System.out.println("Not enough money");
                     }
--- src/test/java/com/techelevator/view/VendingMachineTest.java
+++ src/test/java/com/techelevator/view/VendingMachineTest.java
@@ -1,22 +1,48 @@
 package com.techelevator.view;
 
+import com.techelevator.Drink;
+import com.techelevator.Item;
 import com.techelevator.VendingMachine;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.Assert;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class VendingMachineTest {
 
     private VendingMachine objectOutput;
+    private Map<String, Item> inventory = new HashMap<>();
 
     @Before
     public void new_object() { objectOutput = new VendingMachine(); }
 
     @Test
-    public double test_feed_me_method() {
+    public void test_feed_me_method() {
+    String amountInput = "2";
+    double output = objectOutput.feedMe(amountInput);
+    Assert.assertEquals(2.0, output, 0.0);
 
+    }
 
-    return 0.0;
+    @Test
+    public void test_feed_me_method_unacceptable_string() {
+        String amountInput = "five";
+        double output = objectOutput.feedMe(amountInput);
+        Assert.assertEquals(0.0, output, 0.0);
+
+    }
+
+    @Test
+    public void test_VM_stocked_method() {
+        String inventoryFilePath = "src/test/resources/VM stocked test";
+        Map <String, Item> expected = new HashMap<>();
+        Drink drink = new Drink ("C4", "Heavy", 1.50);
+        expected.put("C4", drink);
+        Map output = objectOutput.getVendingMachineStocked(inventoryFilePath);
+        Assert.assertEquals(expected, output);
+
     }
 
 
